e227bf5cec3201680f1343245bbdc9063f61ae87
i2life <i2life@yeah.net>
2020-02-22 10:50:02 +0800

    create project

---
+++ src/main/java/com/zhiying/moviekg/MoviekgApplication.java
@@ -0,0 +1,13 @@
+package com.zhiying.moviekg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviekgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviekgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhiying/moviekg/MoviekgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhiying.moviekg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviekgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
